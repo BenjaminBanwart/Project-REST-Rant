67aff73e45f3d6064ea66f5df0b1eaff04f44ffa
Benjamin Banwart <benjibanwart@gmail.com>
2022-02-28 19:32:11 -0600

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -9,6 +9,19 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    } 
+    else if(!places[id]) {
+        res.render('error404')
+    } 
+    else {
+        res.render('places/show', { place: places[id] })
+    }
+})
+
 router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
