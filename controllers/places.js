0fca640d01178c60c2fad1cf24c2ec8b586843ee
Benjamin Banwart <benjibanwart@gmail.com>
2022-02-28 20:53:01 -0600

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -52,4 +52,17 @@ router.delete('/:id', (req, res) => {
     }
 })
 
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id] })
+    }
+})
+
 module.exports = router
