43465e9e8dcf7ea177e1384be97aff105dafaea3
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2022-03-05 14:04:27 -0600

    Added database

--- models/places.js
+++ models/places.js
@@ -1,13 +1,12 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/shawnanggg-nmpW_WwwVSc-unsplash.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/michal-parzuchowski-ItaV89TNkks-unsplash.jpg'
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' },
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
