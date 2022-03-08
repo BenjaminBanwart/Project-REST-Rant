6b31ce07679ac5e1d836fa06013a3ca6d7180a39
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2022-03-07 21:33:48 -0600

    Added ability to add mock data

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,27 +7,27 @@ function new_form () {
             <main>
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
-                    <div className='form-group'>
+                    <div className='form-group mx-auto col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor="name">Place Name</label>
                         <input className='form-control' id="name" name="name" required />
                     </div>
-                    <div className='form-group'>
+                    <div className='form-group mx-auto col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor="pic">Place Picture</label>
                         <input className='form-control' id="pic" name="pic" />
                     </div>
-                    <div className='form-group'>
+                    <div className='form-group mx-auto col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor="city">City</label>
                         <input className='form-control' id="city" name="city" />
                     </div>
-                    <div className='form-group'>
+                    <div className='form-group mx-auto col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor="state">State</label>
                         <input className='form-control' id="state" name="state" />
                     </div>
-                    <div className='form-group'>
+                    <div className='form-group mx-auto col-sm-6 col-md-4 col-lg-3'>
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className='form-control' id="cuisines" name="cuisines" required />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label for="founded">Founded Year</label>
                         <input className="form-control" id="founded" name="founded" />
                     </div>
