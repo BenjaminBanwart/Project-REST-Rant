688f1645bfdc4f4650926611bf928b8465fd5a41
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2022-03-07 22:39:36 -0600

    Added validation to create form

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -29,7 +29,7 @@ function new_form () {
                     </div>
                     <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label for="founded">Founded Year</label>
-                        <input className="form-control" id="founded" name="founded" />
+                        <input className="form-control" id="founded" name="founded" type='number' value={new Date().getFullYear()}/>
                     </div>
                         <input className='btn btn-primary' type="submit" value="Add Place" />
                 </form>
